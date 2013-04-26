1a37904f99429608206967d86b22a16caeef4ee6
April Goldman <aprilgoldman@Aprils-MacBook-Air.local>
2013-04-26 16:25:30 -0700

    added conversation view to Message model

--- app/models/message.rb
+++ app/models/message.rb
@@ -6,4 +6,8 @@ class Message < ActiveRecord::Base
   belongs_to :sender, class_name: "User"
   belongs_to :receiver, class_name: "User"
 
+  def conversationView
+  	Message.where("parent_id = :id or id = :id", {id: self.id}).order("created_at ASC")
+  end
+
 end
