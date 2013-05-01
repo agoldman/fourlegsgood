d13810f19fdb2e9a5c9d4f1e3a1282e9c76982a4
April Goldman <aprilgoldman@Aprils-MacBook-Air.local>
2013-05-01 16:40:57 -0700

    sending correct params from map search form

--- app/controllers/current_requests_controller.rb
+++ app/controllers/current_requests_controller.rb
@@ -1,30 +1,63 @@
 class CurrentRequestsController < ApplicationController
 
 	respond_to :json
+	respond_to :html
 
 	def index
-		# @user = User.find(params[:user_id]) #will change to current user when I have login
-		@address = (params[:address])
-		@near_by_users = User.near(@address, 50)
-		@near_by_ids_any_status = []
-		@near_by_request_info = []
-		@near_by_users.each do |user|
-			@near_by_ids_any_status << user.id 
+		
+
+		if (params[:start] && params[:end]) 
+			fail
+
+
+			@address = (params[:address])
+			@near_by_users = User.near(@address, 50)
+			@near_by_ids_any_status = []
+			@near_by_request_info = []
+			@near_by_users.each do |user|
+				@near_by_ids_any_status << user.id 
+			end
+
+			@current_requests = SittingRequest.where(owner_id: @near_by_ids_any_status, status: 'requested')
+			@near_by_ids_requested_status = [];
+			@current_requests.each do |request|
+				@near_by_ids_requested_status << request.owner_id 
+			end
+
+			@current_request_users = User.where(id: @near_by_ids_requested_status)
+			@current_request_users.each do |user|
+				@near_by_request_info << [user.latitude, user.longitude, user.id, user.address, user.user_name]
+			end
+
+
+			render json: @near_by_request_info
+
+
+		else
+			# @user = User.find(params[:user_id]) #will change to current user when I have login
+			@address = (params[:address])
+			@near_by_users = User.near(@address, 50)
+			@near_by_ids_any_status = []
+			@near_by_request_info = []
+			@near_by_users.each do |user|
+				@near_by_ids_any_status << user.id 
+			end
+
+			@current_requests = SittingRequest.where(owner_id: @near_by_ids_any_status, status: 'requested')
+			@near_by_ids_requested_status = [];
+			@current_requests.each do |request|
+				@near_by_ids_requested_status << request.owner_id 
+			end
+
+			@current_request_users = User.where(id: @near_by_ids_requested_status)
+			@current_request_users.each do |user|
+				@near_by_request_info << [user.latitude, user.longitude, user.id, user.address, user.user_name]
+			end
+
+
+			render json: @near_by_request_info
 		end
 
-		@current_requests = SittingRequest.where(owner_id: @near_by_ids_any_status, status: 'requested')
-		@near_by_ids_requested_status = [];
-		@current_requests.each do |request|
-			@near_by_ids_requested_status << request.owner_id 
-		end
-
-		@current_request_users = User.where(id: @near_by_ids_requested_status)
-		@current_request_users.each do |user|
-			@near_by_request_info << [user.latitude, user.longitude, user.id, user.address, user.user_name]
-		end
-
-
-		render json: @near_by_request_info
 	end
 
 end
