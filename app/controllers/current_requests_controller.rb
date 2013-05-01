9d80c3a443d6397491ba9247087dfa4a58453bec
April Goldman <aprilgoldman@Aprils-MacBook-Air.local>
2013-05-01 11:49:15 -0700

    returning nearby users' lat/lngs from server

--- app/controllers/current_requests_controller.rb
+++ app/controllers/current_requests_controller.rb
@@ -7,13 +7,22 @@ class CurrentRequestsController < ApplicationController
 		@address = (params[:address])
 		@near_by_users = User.near(@address, 20)
 		@near_by_ids = []
+		@near_by_addresses = []
 		@near_by_users.each do |user|
 			@near_by_ids << user.id 
 		end
 
 		@current_requests = SittingRequest.where(owner_id: @near_by_ids, status: 'requested')
+		@current_requests.each do |request|
+			@near_by_ids = request.owner_id 
+		end
 
-		render json: @current_requests
+		@current_request_users = User.where(id: @near_by_ids)
+		@current_request_users.each do |user|
+			@near_by_addresses << [user.latitude, user.longitude]
+		end
+
+		render json: @near_by_addresses
 	end
 
 end
--- app/models/sitting_request.rb
+++ app/models/sitting_request.rb
@@ -9,68 +9,6 @@ class SittingRequest < ActiveRecord::Base
   belongs_to :owner, class_name: "User"
 
 
-
-  def self.getNearby(near_by_ids)
-  	query = "SELECT users.address
-            FROM sitting_requests
-            JOIN users
-            ON users.id = sitting_requests.owner_id
-            WHERE sitting_requests.status = 'requested'
-           	AND sitting_requests.owner_id IN #{near_by_ids}"
-            
-
-     ActiveRecord::Base.connection.execute(query);
-
-  end
-
-
-  def self.usersOfRequested
-
-    query = "SELECT *
-	    	 FROM users 
-	   		 JOIN sitting_requests 
-	   		 ON users.id = sitting_requests.owner_id
-			 WHERE sitting_requests.status = 'requested'"
-
-    ActiveRecord::Base.connection.execute(query);
-  end
-
-
-  def self.geocodedAddresses(users)
-  queries = []
-
-  users.each do |user|
-				queries << Addressable::URI.new(
-				  :scheme => "http",
-				  :host => "maps.googleapis.com",
-				  :path => "/maps/api/geocode/json",
-				  :query_values => {:address => "#{user.address}",
-				                     :sensor => "false"
-				                    }
-				  ).to_s
-		end
-	geocode_responses = queries.map do |query|
-		JSON.parse(RestClient.get(query))
-		end
-
-	latlongs = geocode_responses.map do |gc_response|
-		lat = gc_response["results"][0]["geometry"]["location"]["lat"]
-		lng = gc_response["results"][0]["geometry"]["location"]["lng"]
-		[lat, lng]
-	end
-
-	latlongs.each_with_index do |latlng, index|
-		u = users[index]
-		u.lat = latlng[0]
-		u.lng = latlng[1]
-		u.save
-	end
-
-	return latlongs
-end
-
-
-
 end
 
 
