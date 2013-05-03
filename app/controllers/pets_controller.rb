329ee8e7ef197babfbfbd5b6d3e5b4f58f9eb5d8
April Goldman <aprilgoldman@Aprils-MacBook-Air.local>
2013-05-02 18:23:59 -0700

    added images to google infowindow

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -3,9 +3,17 @@ class PetsController < ApplicationController
 	respond_to :json
 
 	def index
-		@owner_id = params[:user_id]
-		@dog = Pet.find_by_owner_id(@owner_id)
-		render json: @dog
+
+		if(params[:user_id])
+			@owner_id = params[:user_id]
+			@dog = Pet.find_by_owner_id(@owner_id)
+			render json: @dog
+		else
+			@owner_id = params[:owner_id]
+			@dog = Pet.find_by_owner_id(@owner_id)
+			p @dog
+			render json: @dog
+		end
 	end
 
 end
