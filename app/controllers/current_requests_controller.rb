82f40e1dc99fe2f8d864453086a5bb629f39797c
April Goldman <aprilgoldman@Aprils-MacBook-Air.local>
2013-05-03 11:15:53 -0700

    debug statements for pins

--- app/controllers/current_requests_controller.rb
+++ app/controllers/current_requests_controller.rb
@@ -35,8 +35,16 @@ class CurrentRequestsController < ApplicationController
 
 		else
 			# @user = User.find(params[:user_id]) #will change to current user when I have login
+
 			@address = (params[:address])
 			@near_by_users = User.near(@address, 100)
+			@all = User.all
+			p"all users"
+			p @all
+
+			p "near_by_users"
+			p @near_by_users
+			
 			@near_by_ids_any_status = []
 			@near_by_request_info = []
 			@near_by_users.each do |user|
@@ -44,16 +52,25 @@ class CurrentRequestsController < ApplicationController
 			end
 
 			@current_requests = SittingRequest.where(owner_id: @near_by_ids_any_status, status: 'requested')
+
+			p "current_requests"
+			p @current_requests
+
 			@near_by_ids_requested_status = [];
 			@current_requests.each do |request|
 				@near_by_ids_requested_status << request.owner_id 
 			end
 
 			@current_request_users = User.where(id: @near_by_ids_requested_status)
+
+			p "current_request_users"
+			p @current_request_users
+
+
 			@current_request_users.each do |user|
 				@near_by_request_info << [user.latitude, user.longitude, user.id, user.address, user.user_name]
 			end
-
+			#debugger
 			render json: @near_by_request_info
 		end
 
