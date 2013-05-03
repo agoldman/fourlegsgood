44f74875d0708cc179e0ac90d7929bb83e4c40d3
April Goldman <aprilgoldman@Aprils-MacBook-Air.local>
2013-05-03 14:22:29 -0700

    changed AR query format to ?

--- app/controllers/current_requests_controller.rb
+++ app/controllers/current_requests_controller.rb
@@ -27,7 +27,9 @@ class CurrentRequestsController < ApplicationController
 				@near_by_ids_any_status << user.id 
 			end
 
-			@current_requests = SittingRequest.where(owner_id: @near_by_ids_any_status, status: 'requested').where("start_date >= datetime('" + @start + "')").where("end_date <= datetime('" + @end + "')")
+			@current_requests = SittingRequest.where(owner_id: @near_by_ids_any_status, status: 'requested')
+											  .where("start_date >= ?", @start)
+											  .where("end_date <= ?", @end)
 
 			p "current_requests"
 			p @current_requests
