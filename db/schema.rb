a323ffac51471ca7e8f76a0f85815111b3501964
April Goldman <aprilgoldman@Aprils-MacBook-Air.local>
2013-05-04 15:24:39 -0700

    removed devise

--- db/schema.rb
+++ db/schema.rb
@@ -104,28 +104,20 @@ ActiveRecord::Schema.define(:version => 20130503022417) do
   add_index "swap_exchanges", ["swap_requester_id"], :name => "index_swap_exchanges_on_swap_requester_id"
 
   create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
+    t.string   "email"
+    t.string   "password"
     t.string   "user_name"
     t.text     "about_me"
     t.string   "address"
     t.decimal  "sitter_rate"
     t.integer  "dog_karma"
     t.integer  "sitter_karma"
-    t.integer  "swaps_earned",           :default => 1
+    t.integer  "swaps_earned",        :default => 1
     t.integer  "swap_price"
     t.float    "longitude"
     t.float    "latitude"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.datetime "created_at",                         :null => false
+    t.datetime "updated_at",                         :null => false
     t.string   "avatar_file_name"
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
@@ -133,6 +125,5 @@ ActiveRecord::Schema.define(:version => 20130503022417) do
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
