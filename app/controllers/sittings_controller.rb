7c37339b3cfdba6fa70c9e7a2a151b50ea1e807a
April Goldman <aprilgoldman@unknown008865439b8a.att.net>
2013-05-10 16:22:17 -0700

    sittings controller

--- app/controllers/sittings_controller.rb
+++ app/controllers/sittings_controller.rb
@@ -9,6 +9,13 @@ class SittingsController < ApplicationController
 		else
 			@sitting = Sitting.new
 			@sitting.sitter_id = current_user.id
+			@request = SittingRequest.find(params[:sitting_request][:id])
+			@sitting.start_date = @request.start_date
+			@sitting.end_date = @request.end_date
+			@sitting.status = 'offered'
+			@sitting.sat_for_owner_id = @request.owner_id
+			@sitting.save # add check for success here
+			redirect_to current_request_url(@request.id)
 		end
 	end
 
